487ecfaac26b19070630fa77b6ab31928f7ed9c5
dibuj <00371147@anahuac.mx>
2022-06-30 19:45:34 -0500

    commit

--- src/test/java/com/fca/calidad/unittest/testGoogleFunc.java
+++ src/test/java/com/fca/calidad/unittest/testGoogleFunc.java
@@ -17,7 +17,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-	
+	//
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class testGoogleFunc {
